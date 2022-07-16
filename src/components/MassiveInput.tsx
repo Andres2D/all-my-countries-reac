b83f363b3d7bbbeec23dc81bba9d8ce2b2ead6dd
Andres2D <andres2d1997.1206@gmail.com>
2022-07-15 23:42:20 -0500

    Add search input component

---
+++ src/components/MassiveInput.tsx
@@ -0,0 +1,19 @@
+import styles from './MassiveInput.module.css';
+
+interface Props {
+  placeholder: string;
+  onChange: (event: React.FormEvent<HTMLInputElement>) => void;
+}
+
+const MassiveInput: React.FC<Props> = ({placeholder, onChange}) => {
+  return (
+    <input
+      className={styles.search} 
+      type='text' 
+      placeholder={placeholder} 
+      onChange={onChange}
+    />
+  )
+};
+
+export default MassiveInput;
