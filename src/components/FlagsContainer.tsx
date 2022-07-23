b4c3fea19b3a872684de58546f7f10b90de15a84
Andres2D <andres2d1997.1206@gmail.com>
2022-07-22 22:08:45 -0500

    Add maps links

--- src/components/FlagsContainer.tsx
+++ src/components/FlagsContainer.tsx
@@ -4,9 +4,11 @@ interface Props {
   nationalFlag?: string;
   coatOfArmsFlag?: string;
   countryName?: string;
+  googleMapsLink?: string;
+  openStreetMapsLink?: string;
 }
 
-const FlagsContainer: React.FC<Props> = ({nationalFlag, coatOfArmsFlag, countryName}) => {
+const FlagsContainer: React.FC<Props> = ({nationalFlag, coatOfArmsFlag, countryName, googleMapsLink, openStreetMapsLink}) => {
   return (
     <div className={styles.flags}>
       <div className={styles.country_flag}>
@@ -16,6 +18,10 @@ const FlagsContainer: React.FC<Props> = ({nationalFlag, coatOfArmsFlag, countryN
       <div className={styles.army_flag}>
         <p className={styles.flag_label}>Coat of arms</p>
         <img className={styles.flag} src={coatOfArmsFlag} alt={`coa_${countryName}`} />
+      </div>
+      <div className={styles.maps}>
+        {googleMapsLink && <a className={styles.map} rel='noreferrer' href={googleMapsLink} target='_blank'>Google Maps</a>}
+        {openStreetMapsLink && <a className={styles.map} rel='noreferrer' href={openStreetMapsLink} target='_blank'>OpenStreet</a>}
       </div>
     </div> 
   );
