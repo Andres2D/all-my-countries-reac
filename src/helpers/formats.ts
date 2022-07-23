fc0e2bf275cd7b5d4a7b5a5cdc4a6d8dae99ac85
Andres2D <andres2d1997.1206@gmail.com>
2022-07-22 21:46:34 -0500

    Add helpers functions

---
+++ src/helpers/formats.ts
@@ -0,0 +1,64 @@
+import { 
+  Currencies, 
+  Demonyms,
+  Idd,
+  Languages,
+  Translations
+} from '../interfaces/country.interface';
+
+export const currencyFormat = (currencies: Currencies): string => {
+  let currencyFormatted: string[] = [];
+
+  Object.values(currencies).forEach(currency => {
+    currencyFormatted.push(`${currency.name} (${currency.symbol})`);
+  })
+
+  return currencyFormatted.join('|');
+};
+
+export const demonymsFormat = (demonyms: Demonyms): string => {
+  let demonymsList: string[] = [];
+  Object.values(demonyms).forEach(({f, m}) => {
+    if(f && !demonymsList.includes(f)) {
+      demonymsList.push(f);
+    }
+    if(m && !demonymsList.includes(m)) {
+      demonymsList.push(m);
+    }
+  });
+
+  return demonymsList.join(' | ');
+};
+
+export const phoneCodesFormat = (idd: Idd): string => {
+  const { root, suffixes } = idd;
+
+  const suffixesFormatted = suffixes?.map(sub => {
+    return `${root}${sub}`;
+  });
+
+  return suffixesFormatted ? suffixesFormatted?.join(' | ') : '';
+}
+
+export const languagesFormat = (languages: Languages): string => {
+
+  let languagesFormatted: string[] = [];
+
+  Object.values(languages).forEach(language => {
+    languagesFormatted.push(language);
+  });
+
+  return languagesFormatted.join(' | ');
+};
+
+export const translationsFormat = (translations: Translations): string[] => {
+  let translationsFormatted: string[] = [];
+
+  Object.values(translations).forEach(({common}) => {
+    if(common && !translationsFormatted.includes(common)) {
+      translationsFormatted.push(common); 
+    }
+  })
+
+  return translationsFormatted;
+};
